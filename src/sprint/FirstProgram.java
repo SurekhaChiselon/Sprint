baff7176fddc4cc2ebc8987a16c0887fe223cc8b
SurekhaChiselon <surekha.satya@gmail.com>
2018-08-30 11:21:38 +0530

    First Commit

---
+++ src/sprint/FirstProgram.java
@@ -0,0 +1,13 @@
+package sprint;
+
+public class FirstProgram {
+
+	public static void main(String[] args) 
+	{
+      System.out.println("Hi Hello");
+      System.out.println(System.getProperty("java.runtime.version"));
+      System.out.println(System.getProperty("sun.arch.data.model"));
+
+	}
+
+}
